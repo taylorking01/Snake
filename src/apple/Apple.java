d0b0a9aeb48ddc744a0856e0a4293eec66dc0074
taylorking01 <taylorkinginbox@gmail.com>
2024-10-22 17:20:25 +0100

    Formatted apple class

--- src/apple/Apple.java
+++ src/apple/Apple.java
@@ -1,14 +1,17 @@
 package apple;
 
 public class Apple {
-    private int x;
-    private int y;
+    private int x; //column of apple
+    private int y; //row of apple
 
     public Apple(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
+    /**
+     * Getter functions for the apple
+     */
     public int getX() {
         return x;
     }
